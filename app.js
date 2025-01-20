43cd32f9fe55cc44ed3500dc2ee6136adf77956a
Ruanpbo <ruanpbo@hotmail.com>
2025-01-20 17:19:07 -0300

    Adicionando função

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Jogo do numero secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+
+function verificarChute(){
+    
+}
