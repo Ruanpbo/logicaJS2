3097e8be7c1d236c909a4a6a54a47cb6b40a9a7d
Ruanpbo <ruanpbo@hotmail.com>
2025-01-20 18:58:10 -0300

    Adicionando funcionalidade ao botão de verificar

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do numero secreto';
+let numeroSecreto = nAleatorio();
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
+function textos(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+textos('h1', 'Jogo do numero secreto');
+textos('p', 'Escolha um numero entre 1 e 10');
+
+function nAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
+}
+
+let msg;
+let tentativas = 1;
 
 function verificarChute(){
     
+    let chute = document.querySelector('input').value;
+        if (chute == numeroSecreto) {
+            tentativas > 1 ? msg='Tentativas' : 'tentativa';
+            alert(`Você acertou o numero secreto é ${numeroSecreto}, você acertou com ${tentativas} ${msg}`); 
+        } else {
+            chute > numeroSecreto ? alert(`O numero secreto é menor que ${chute}`) : alert(`O numero secreto é maior que ${chute}`);
+            tentativas++
+        }
+    
 }
+
