dc581a7190d497f6f7c377bbcd2ca7f876233d71
Ruanpbo <ruanpbo@hotmail.com>
2025-01-21 17:33:26 -0300

    Jogo pronto

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ function textos(tag, texto){
     campo.innerHTML = texto;
 }
 
-textos('h1', 'Jogo do numero secreto');
-textos('p', 'Escolha um numero entre 1 e 10');
+    function initmsg(){
+        textos('h1', 'Jogo do numero secreto');
+        textos('p', 'Escolha um numero entre 1 e 10');
+    }
+    
+    initmsg();
 
 function nAleatorio(){
     return parseInt(Math.random() * 10 + 1);
@@ -19,12 +23,27 @@ function verificarChute(){
     
     let chute = document.querySelector('input').value;
         if (chute == numeroSecreto) {
-            tentativas > 1 ? msg='Tentativas' : 'tentativa';
-            alert(`Você acertou o numero secreto é ${numeroSecreto}, você acertou com ${tentativas} ${msg}`); 
+            tentativas > 1 ? msg='tentativas' : 'tentativa';
+            textos('h1',`Você acertou o numero secreto é ${numeroSecreto}, você acertou com ${tentativas} ${msg}`);
+            textos('p',`Parabéns &#129395 &#129395 &#129395` );
+            document.getElementById('reiniciar').removeAttribute('disabled'); 
         } else {
-            chute > numeroSecreto ? alert(`O numero secreto é menor que ${chute}`) : alert(`O numero secreto é maior que ${chute}`);
+            chute > numeroSecreto ? textos('p',`O numero secreto é menor que ${chute}`) : textos('p',`O numero secreto é maior que ${chute}`);
             tentativas++
+            limpar()
         }
     
 }
 
+function limpar(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(){
+    numeroSecreto = nAleatorio();
+    limpar();
+    initmsg();
+    tentativas = 1;
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
